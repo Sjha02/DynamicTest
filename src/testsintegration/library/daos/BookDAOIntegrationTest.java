fb4c3fe0660740fdebb9846fa7f942a869f64956
Sjha02 <shae.jha@gmail.com>
2015-10-26 05:18:52 +1100

    Low Level Integration Test Completed for entities and DAO

---
+++ src/testsintegration/library/daos/BookDAOIntegrationTest.java
@@ -0,0 +1,197 @@
+package testsintegration.library.daos;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import library.daos.BookMapDAO;
+import library.daos.BookHelper;
+import library.entities.Book;
+import library.interfaces.daos.IBookDAO;
+import library.interfaces.daos.IBookHelper;
+import library.interfaces.entities.IBook;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class BookDAOIntegrationTest {
+	
+	private BookMapDAO bookDAO;
+	private IBookHelper helper;
+	private String author;
+	private String title;
+	private String callNumber;
+
+	@Before
+	public void setUp() throws Exception {
+		
+		
+		author = "Jim T";
+		title = "itc205";
+		callNumber = "88";
+		
+		
+		helper = new BookHelper();
+		bookDAO  = new BookMapDAO (helper);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		helper = null;
+		bookDAO = null;
+	}
+
+	@Test
+	public void testBookDAO() {
+		assertNotNull(helper);
+		assertTrue(bookDAO instanceof IBookDAO);
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testBookDAONull() {
+		bookDAO = new BookMapDAO(null);
+	}
+	
+	@Test
+	public void testAddBook() {
+		IBook book = bookDAO.addBook(author, title, callNumber);
+		assertNotNull(book);
+		assertTrue(book == bookDAO.findBooksByAuthor(author).get(0));
+	}
+	
+	@Test
+	public void testGetBookByID() {
+		IBook book = bookDAO.addBook(author, title, callNumber);
+		assertNotNull(book);
+		assertTrue(book == bookDAO.getBookByID(book.getID()));
+	}
+	
+	@Test
+	public void testGetBookByIDFail() {
+		IBook book = helper.makeBook(author, title, callNumber, 1);
+		assertNotNull(book);
+		assertTrue(bookDAO.getBookByID(book.getID()) == null);
+	}
+	
+	@Test
+	public void listBooks() {
+		IBook book = bookDAO.addBook(author, title, callNumber);
+		for (IBook e : bookDAO.listBooks()) {
+			if (book == e) {
+				assertTrue(book == e);
+			}
+			else fail("Match was found");
+		}
+	}
+	
+
+	@Test
+	public void testFindBooksByAuthor() {
+		IBook book = bookDAO.addBook(author, title, callNumber);
+		for (IBook e : bookDAO.findBooksByAuthor(author)) {
+			if (book.getAuthor() == e.getAuthor()) {
+				assertTrue(book.getAuthor() == e.getAuthor());
+			}
+			else fail("Match was found");
+		}
+	}
+	
+	@Test
+	public void testFindBooksByAuthorNoMatch() {
+		bookDAO.addBook(author, title, callNumber);
+		String authorTemp = "Joe";
+		for (IBook e : bookDAO.findBooksByAuthor(author)) {
+			if (e.getAuthor() != authorTemp) {
+				assertTrue(e.getAuthor() != authorTemp);
+			}
+			else fail("Match was found");
+		}
+	}
+
+	@Test
+	public void testFindBooksByTitle() {
+		IBook book = bookDAO.addBook(author, title, callNumber);
+		for (IBook e : bookDAO.findBooksByTitle(title)) {
+			if (book.getTitle() == e.getTitle()) {
+				assertTrue(book.getTitle() == e.getTitle());
+			}
+			else fail("Match was found");
+		}
+	}
+	
+	@Test
+	public void testFindBooksByTitleNoMatch() {
+		bookDAO.addBook(author, title, callNumber);
+		String titleTemp = "Maths 201";
+		for (IBook e : bookDAO.findBooksByTitle(title)) {
+			if (e.getTitle() != titleTemp) {
+				assertTrue(e.getTitle() != titleTemp);
+			}
+			else fail("Match was found");
+		}
+	}
+
+	@Test
+	public void testFindBooksByAuthorTitle() {
+		IBook book = bookDAO.addBook(author, title, callNumber);
+		for (IBook e : bookDAO.findBooksByAuthorTitle(author, title)) {
+			if (book.getTitle() == e.getTitle()) {
+				assertTrue(book.getTitle() == e.getTitle());
+			}
+			else {
+				throw new RuntimeException("");
+				}
+			if (book.getAuthor() == e.getAuthor()) {
+				assertTrue(book.getAuthor() == e.getAuthor());
+			}
+			else {
+				throw new RuntimeException("");
+			}
+		}
+	}
+	
+	@Test
+	public void testFindBooksByAuthorTitleNoMatchTitle() {
+		bookDAO.addBook(author, title, callNumber);
+		String titleTemp = "Maths 201";
+		for (IBook e : bookDAO.findBooksByTitle(title)) {
+			if (e.getTitle() != titleTemp) {
+				assertTrue(e.getTitle() != titleTemp);
+			}
+			else fail("Match was found");
+		}
+	}
+	
+	@Test
+	public void testFindBooksByAuthorTitleNoMatchAuthor() {
+		bookDAO.addBook(author, title, callNumber);
+		String authorTemp = "Joe";
+		for (IBook e : bookDAO.findBooksByAuthor(author)) {
+			if (e.getAuthor() != authorTemp) {
+				assertTrue(e.getAuthor() != authorTemp);
+			}
+			else fail("Match was found");
+		}
+	}
+	
+	@Test
+	public void testFindBooksByAuthorTitleNoMatches() {
+		bookDAO.addBook(author, title, callNumber);
+		String titleTemp = "Maths 201";
+		String authorTemp = "Joe";
+		for (IBook e : bookDAO.findBooksByAuthor(author)) {
+			if (e.getTitle() != titleTemp) {
+				assertTrue(e.getTitle() != titleTemp);
+			}
+			else {
+				fail("Match was found");
+			}
+			if (e.getAuthor() != authorTemp) {
+					assertTrue(e.getAuthor() != authorTemp);
+				}
+			else fail("Match was found");
+		}
+	}
+
+}
---
+++ src/testsintegration/library/entites/MemberEntityIntegrationTest.java
@@ -0,0 +1,143 @@
+package testsintegration.library.entites;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import library.entities.Book;
+import library.entities.Loan;
+import library.entities.Member;
+import library.interfaces.entities.EMemberState;
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.ILoan;
+import library.interfaces.entities.IMember;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class MemberEntityIntegrationTest {
+	private IBook book;
+	private String author;
+	private String title;
+	private String callNumber;
+	private int bookID;
+	
+	private IMember borrower;
+	private String fName;
+	private String lName;
+	private String contact;
+	private String email;
+	private int memberID;
+	
+	private ILoan loan;
+	private int loanID;
+	private Date borrowDate;
+	private Date dueDate;
+	private Calendar calendar;
+
+	@Before
+	public void setUp() throws Exception {
+		author = "Jim T";
+		title = "itc205";
+		callNumber = "88";
+		bookID = 10;
+		book = new Book(author, title, callNumber, bookID);
+		
+		fName = "Rob";
+		lName = "Jones";
+		contact = "0430283468";
+		email = "skj@gmail.com";
+		memberID = 01;
+		borrower = new Member(fName, lName, contact, email, memberID);
+		
+		loanID = 1;
+		calendar = Calendar.getInstance();
+		borrowDate = new Date();
+		calendar.setTime(borrowDate);
+		calendar.add(Calendar.DATE, ILoan.LOAN_PERIOD);
+		dueDate = calendar.getTime();
+		loan = new Loan(book, borrower, borrowDate, dueDate);
+		loan.commit(loanID);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		book = null;
+		borrower = null;
+		loan = null;
+	}
+
+	@Test
+	public void testMember() {
+		assertNotNull(borrower);
+		assertTrue(borrower instanceof IMember);
+	}
+	
+	@Test
+	public void testHasOverDueLoans() {
+		assertTrue(borrower.hasOverDueLoans() == false);
+	}
+	
+	@Test
+	public void testHasOverDueLoansTrue() {
+		Calendar falseCalendar = Calendar.getInstance();
+		falseCalendar.add(Calendar.YEAR, 1000);
+		Date falseDate = falseCalendar.getTime();
+		loan.checkOverDue(falseDate);
+		assertTrue(borrower.hasOverDueLoans() == true);
+	}
+	
+	@Test
+	public void hasReachedLoanLimit() {
+		assertTrue(borrower.hasReachedLoanLimit() == false);
+	}
+	
+	@Test
+	public void hasReachedLoanLimitTrue() {
+		borrower.addLoan(loan);
+		borrower.addLoan(loan);
+		borrower.addLoan(loan);
+		borrower.addLoan(loan);
+		assertTrue(borrower.hasReachedLoanLimit() == true);
+	}
+	
+	@Test
+	public void addLoan() {
+		borrower.addLoan(loan);
+		assertTrue(loan == borrower.getLoans().get(1));
+	}
+	
+	@Test (expected=RuntimeException.class)
+	public void addLoanFailDueToBorrowingLimitReached() {
+		borrower.addLoan(loan);
+		borrower.addLoan(loan);
+		borrower.addLoan(loan);
+		borrower.addLoan(loan);
+		borrower.addLoan(loan);
+		assertTrue(borrower.getState() == EMemberState.BORROWING_DISALLOWED);
+	}
+	
+	@Test
+	public void getLoans() {
+		borrower.addLoan(loan);
+		assertTrue(loan == borrower.getLoans().get(1));
+	}
+	
+	@Test
+	public void removeLoan() {
+		ILoan loan2 = new Loan(book, borrower, borrowDate, dueDate);
+		borrower.addLoan(loan2);
+		borrower.removeLoan(loan2);
+		assertTrue(loan2 != borrower.getLoans().get(0));
+	}
+	
+	@Test (expected=RuntimeException.class)
+	public void removeLoanNullLoans() {
+		ILoan loan2 = new Loan(book, borrower, borrowDate, dueDate);
+		borrower.removeLoan(loan2);
+	}
+	
+}
